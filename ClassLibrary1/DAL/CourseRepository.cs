7510f8570e33c2aa01d819c01913fcb5465a193e
rumeshwart <rumesh.wart@gmail.com>
2014-12-17 18:31:32 +0530

    3rd commit

---
+++ ClassLibrary1/DAL/CourseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.DAL
+{
+    public class CourseRepository:ICourseDataAccss
+    {
+        public void AddNew(Course c)
+        {
+            //context.courses.add();
+            //context,savechange();
+        }
+    }
+}
---
+++ ClassLibrary1/Servises/CourseServise.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Servises
+{
+    class CourseServise:ICourseServise
+    {
+
+        public CourseServise()
+        {
+
+        }
+
+        public void addNewCourse(Course c)
+        {
+          
+        }
+
+        public void validate(Course c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
